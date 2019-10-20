e015805b8d5b7d1c06109ba3ea65d20eabfbd80a
MohanadAB <54993284+MohanadAB@users.noreply.github.com>
2019-10-21 00:56:46 +0300

    Add files via upload

---
+++ Project COD Plutonium/Plutonium.js
@@ -0,0 +1,32 @@
+alert("Welcome To Plutonium");
+/*
+var smallPrice = 250,
+	meduimPrice = 250,
+	bigPrice = smallPrice + meduimPrice;
+document.getElementById("main").innerHTML = bigPrice;
+var myName = "Mohanad";
+var myAge = 20;
+var myCountry = "Gaza";
+var myDiv = document.getElementById("javascript");
+myDiv.innerHTML = 
+				  "My Name is: " + myName + "<br>" +
+				  "My Age is: " + myAge + "<br>" +
+				  "My Countrjy is: " + myCountry;
+var a = 100
+a++ or --
+*/
+/*
+--------if & else-------
+var yourAge = prompt("Whats Your Age");
+	if (yourAge > 18) {
+		documet.getElementById("test").innerHTML =
+		"welcome To My Website" + yourAge + "+18"
+		console.log("your age +18");
+	}
+	
+	else { 
+			documet.getElementById("test").innerHTML =
+			"sorry" + yourAge + "not +18"
+			console.log("your age not +18");
+	}
+*/
